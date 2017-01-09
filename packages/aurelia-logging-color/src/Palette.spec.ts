8f9b2c5a5e7f1b45f22c91705f9e04b8dea30628
Homa Wong <homawong@gmail.com>
2017-01-08 17:02:03 -0800

    Add none color test

--- packages/aurelia-logging-color/src/Palette.spec.ts
+++ packages/aurelia-logging-color/src/Palette.spec.ts
@@ -19,6 +19,13 @@ test('background', t => {
     actual2.push(pb.color(i.toString()))
   }
   t.deepEqual(actual2, ['padding: 2px; margin: 2px; line-height: 1.8em;background: #96005a;bother: 1px solid #76003a;color: #ffffff;', 'padding: 2px; margin: 2px; line-height: 1.8em;background: #0000c8;bother: 1px solid #0000a8;color: #ffffff;', 'padding: 2px; margin: 2px; line-height: 1.8em;background: #0019ff;bother: 1px solid #0000df;color: #000000;', 'padding: 2px; margin: 2px; line-height: 1.8em;background: #0019ff;bother: 1px solid #0000df;color: #000000;', 'padding: 2px; margin: 2px; line-height: 1.8em;background: #0098ff;bother: 1px solid #0078df;color: #000000;', 'padding: 2px; margin: 2px; line-height: 1.8em;background: #2cff96;bother: 1px solid #0cdf76;color: #000000;', 'padding: 2px; margin: 2px; line-height: 1.8em;background: #97ff00;bother: 1px solid #77df00;color: #000000;', 'padding: 2px; margin: 2px; line-height: 1.8em;background: #ffea00;bother: 1px solid #dfca00;color: #000000;', 'padding: 2px; margin: 2px; line-height: 1.8em;background: #ffea00;bother: 1px solid #dfca00;color: #000000;', 'padding: 2px; margin: 2px; line-height: 1.8em;background: #ff6f00;bother: 1px solid #df4f00;color: #000000;', 'padding: 2px; margin: 2px; line-height: 1.8em;background: #96005a;bother: 1px solid #76003a;color: #ffffff;'])
+
+  const pn = new Palette({ colorMode: 'NONE' })
+  let actual3: string[] = []
+  for (let i = 0; i <= 10; i++) {
+    actual3.push(pn.color(i.toString()))
+  }
+  t.deepEqual(actual3, ['0','1','2','3','4','5','6','7','8','9','10'])
 })
 
 test('text', t => {
