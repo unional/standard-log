16d43424f4f3b632f9abf7b4ad12609d63b60e56
Homa Wong <homawong@gmail.com>
2017-01-08 16:56:42 -0800

    Fix firefox detection

--- packages/aurelia-logging-color/src/environments.ts
+++ packages/aurelia-logging-color/src/environments.ts
@@ -3,7 +3,7 @@ const vendor = typeof navigator !== 'undefined' ? navigator.vendor : undefined
 
 // alternatively check `!!window.chrome`
 const isChrome = userAgent && vendor ? /Chrome/.test(userAgent) && /Google Inc/.test(vendor) : false
-const isFirefox = userAgent ? /firefix/i.test(userAgent) : false
+const isFirefox = userAgent ? /firefox/i.test(userAgent) : false
 
 const isNode = typeof module !== 'undefined' && module.exports
 
