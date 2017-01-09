b7b4e65666bea6152b9e9822e24a626dbe5c614c
Homa Wong <homawong@gmail.com>
2017-01-08 17:40:36 -0800

    Make order more stands out

--- packages/aurelia-logging-color/src/ColorAppender.spec.ts
+++ packages/aurelia-logging-color/src/ColorAppender.spec.ts
@@ -8,9 +8,9 @@ test('color cycling', t => {
   setLevel(logLevel.debug)
   for (let i = 0; i < 25; i++) {
     let log = getLogger(`logger ${i}`)
-    log.debug('debug', Date.now())
-    log.info('info', Date.now())
-    log.warn('warn', Date.now())
-    log.error('error', Date.now())
+    log.debug('x    ', Date.now())
+    log.info(' x  ', Date.now())
+    log.warn('  x ', Date.now())
+    log.error('   x', Date.now())
   }
 })
