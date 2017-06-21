5d0523e030d36cea1c71354550ddef93cafdda48
Snehal Shah <srshah19@users.noreply.github.com>
2017-06-20 22:00:10 -0700

    Adding function to create contrast for bg color, modify tests (#4)
    * Adding function to create contrast for bg color, modify tests, minor spell check fixes
    
    * fixing lint errors
    
    * Addressing Homa's comment for fixing indentation
    
    * Minor update to PR
    
    * Update Ansi16mBrush.ts
    
    Update styling
    
    * Update Ansi16mBrush.ts
    
    Fix again.
    
    * Update CSSBrush.ts
    
    Update style
    
    * Update CSSBrush.ts

--- packages/aurelia-logging-color/src/CSSBrush.spec.ts
+++ packages/aurelia-logging-color/src/CSSBrush.spec.ts
@@ -4,9 +4,9 @@ import test from 'ava'
 import { CSSBrush } from './CSSBrush'
 
 test('work with null', t => {
-  const brush = new CSSBrush({ maxColor: 10 })
-  const actual = brush.color('a', null)
-  t.deepEqual(actual, ["%c a ", "padding: 2px; margin: 2px; line-height: 1.8em;background: #96005a;bother: 1px solid #76003a;color: #ffffff;", null])
+  const brush = new CSSBrush({ maxColor: 10 });
+  const actual = brush.color('a', null);
+  t.deepEqual(actual, ['%c a ', 'padding: 2px; margin: 2px; line-height: 1.8em;background: #96005a;border: 1px solid #76003a;color: #ffffff;', null])
 })
 
 test('background', t => {
@@ -17,7 +17,7 @@ test('background', t => {
     actual.push(brush.color(i.toString()))
   }
 
-  t.deepEqual(actual, [['%c 0 ', 'padding: 2px; margin: 2px; line-height: 1.8em;background: #96005a;bother: 1px solid #76003a;color: #ffffff;'], ['%c 1 ', 'padding: 2px; margin: 2px; line-height: 1.8em;background: #0000c8;bother: 1px solid #0000a8;color: #ffffff;'], ['%c 2 ', 'padding: 2px; margin: 2px; line-height: 1.8em;background: #0019ff;bother: 1px solid #0000df;color: #000000;'], ['%c 3 ', 'padding: 2px; margin: 2px; line-height: 1.8em;background: #0019ff;bother: 1px solid #0000df;color: #000000;'], ['%c 4 ', 'padding: 2px; margin: 2px; line-height: 1.8em;background: #0098ff;bother: 1px solid #0078df;color: #000000;'], ['%c 5 ', 'padding: 2px; margin: 2px; line-height: 1.8em;background: #2cff96;bother: 1px solid #0cdf76;color: #000000;'], ['%c 6 ', 'padding: 2px; margin: 2px; line-height: 1.8em;background: #97ff00;bother: 1px solid #77df00;color: #000000;'], ['%c 7 ', 'padding: 2px; margin: 2px; line-height: 1.8em;background: #ffea00;bother: 1px solid #dfca00;color: #000000;'], ['%c 8 ', 'padding: 2px; margin: 2px; line-height: 1.8em;background: #ffea00;bother: 1px solid #dfca00;color: #000000;'], ['%c 9 ', 'padding: 2px; margin: 2px; line-height: 1.8em;background: #ff6f00;bother: 1px solid #df4f00;color: #000000;'], ['%c 10 ', 'padding: 2px; margin: 2px; line-height: 1.8em;background: #96005a;bother: 1px solid #76003a;color: #ffffff;']])
+  t.deepEqual(actual, [['%c 0 ', 'padding: 2px; margin: 2px; line-height: 1.8em;background: #96005a;border: 1px solid #76003a;color: #ffffff;'], ['%c 1 ', 'padding: 2px; margin: 2px; line-height: 1.8em;background: #0000c8;border: 1px solid #0000a8;color: #ffffff;'], ['%c 2 ', 'padding: 2px; margin: 2px; line-height: 1.8em;background: #0019ff;border: 1px solid #0000df;color: #ffffff;'], ['%c 3 ', 'padding: 2px; margin: 2px; line-height: 1.8em;background: #0019ff;border: 1px solid #0000df;color: #ffffff;'], ['%c 4 ', 'padding: 2px; margin: 2px; line-height: 1.8em;background: #0098ff;border: 1px solid #0078df;color: #ff6700;'], ['%c 5 ', 'padding: 2px; margin: 2px; line-height: 1.8em;background: #2cff96;border: 1px solid #0cdf76;color: #d30069;'], ['%c 6 ', 'padding: 2px; margin: 2px; line-height: 1.8em;background: #97ff00;border: 1px solid #77df00;color: #6800ff;'], ['%c 7 ', 'padding: 2px; margin: 2px; line-height: 1.8em;background: #ffea00;border: 1px solid #dfca00;color: #0015ff;'], ['%c 8 ', 'padding: 2px; margin: 2px; line-height: 1.8em;background: #ffea00;border: 1px solid #dfca00;color: #0015ff;'], ['%c 9 ', 'padding: 2px; margin: 2px; line-height: 1.8em;background: #ff6f00;border: 1px solid #df4f00;color: #0090ff;'], ['%c 10 ', 'padding: 2px; margin: 2px; line-height: 1.8em;background: #96005a;border: 1px solid #76003a;color: #ffffff;']])
 })
 
 test('text', t => {
@@ -27,11 +27,11 @@ test('text', t => {
   for (let i = 0; i <= 10; i++) {
     actual.push(brush.color(i.toString()))
   }
-  t.deepEqual(actual, [['%c 0 ', 'padding: 2px; margin: 2px; line-height: 1.8em;background: #96005a;bother: 1px solid #76003a;color: #ffffff;'], ['%c 1 ', 'padding: 2px; margin: 2px; line-height: 1.8em;background: #0000c8;bother: 1px solid #0000a8;color: #ffffff;'], ['%c 2 ', 'padding: 2px; margin: 2px; line-height: 1.8em;background: #0019ff;bother: 1px solid #0000df;color: #000000;'], ['%c 3 ', 'padding: 2px; margin: 2px; line-height: 1.8em;background: #0019ff;bother: 1px solid #0000df;color: #000000;'], ['%c 4 ', 'padding: 2px; margin: 2px; line-height: 1.8em;background: #0098ff;bother: 1px solid #0078df;color: #000000;'], ['%c 5 ', 'padding: 2px; margin: 2px; line-height: 1.8em;background: #2cff96;bother: 1px solid #0cdf76;color: #000000;'], ['%c 6 ', 'padding: 2px; margin: 2px; line-height: 1.8em;background: #97ff00;bother: 1px solid #77df00;color: #000000;'], ['%c 7 ', 'padding: 2px; margin: 2px; line-height: 1.8em;background: #ffea00;bother: 1px solid #dfca00;color: #000000;'], ['%c 8 ', 'padding: 2px; margin: 2px; line-height: 1.8em;background: #ffea00;bother: 1px solid #dfca00;color: #000000;'], ['%c 9 ', 'padding: 2px; margin: 2px; line-height: 1.8em;background: #ff6f00;bother: 1px solid #df4f00;color: #000000;'], ['%c 10 ', 'padding: 2px; margin: 2px; line-height: 1.8em;background: #96005a;bother: 1px solid #76003a;color: #ffffff;']])
+  t.deepEqual(actual, [['%c 0 ', 'padding: 2px; margin: 2px; line-height: 1.8em;background: #96005a;border: 1px solid #76003a;color: #ffffff;'], ['%c 1 ', 'padding: 2px; margin: 2px; line-height: 1.8em;background: #0000c8;border: 1px solid #0000a8;color: #ffffff;'], ['%c 2 ', 'padding: 2px; margin: 2px; line-height: 1.8em;background: #0019ff;border: 1px solid #0000df;color: #ffffff;'], ['%c 3 ', 'padding: 2px; margin: 2px; line-height: 1.8em;background: #0019ff;border: 1px solid #0000df;color: #ffffff;'], ['%c 4 ', 'padding: 2px; margin: 2px; line-height: 1.8em;background: #0098ff;border: 1px solid #0078df;color: #ff6700;'], ['%c 5 ', 'padding: 2px; margin: 2px; line-height: 1.8em;background: #2cff96;border: 1px solid #0cdf76;color: #d30069;'], ['%c 6 ', 'padding: 2px; margin: 2px; line-height: 1.8em;background: #97ff00;border: 1px solid #77df00;color: #6800ff;'], ['%c 7 ', 'padding: 2px; margin: 2px; line-height: 1.8em;background: #ffea00;border: 1px solid #dfca00;color: #0015ff;'], ['%c 8 ', 'padding: 2px; margin: 2px; line-height: 1.8em;background: #ffea00;border: 1px solid #dfca00;color: #0015ff;'], ['%c 9 ', 'padding: 2px; margin: 2px; line-height: 1.8em;background: #ff6f00;border: 1px solid #df4f00;color: #0090ff;'], ['%c 10 ', 'padding: 2px; margin: 2px; line-height: 1.8em;background: #96005a;border: 1px solid #76003a;color: #ffffff;']])
 })
 
 test('multiple colors', t => {
   const brush = new CSSBrush()
   const actual = brush.color('myLogger', '%c SomeColored Text %c AnotherColor', 'color:blue;', 'color:red;', 'not colored')
-  t.deepEqual(actual, ['%c myLogger %c SomeColored Text %c AnotherColor', 'padding: 2px; margin: 2px; line-height: 1.8em;background: #96005a;bother: 1px solid #76003a;color: #ffffff;', 'color:blue;', 'color:red;', 'not colored'])
+  t.deepEqual(actual, ['%c myLogger %c SomeColored Text %c AnotherColor', 'padding: 2px; margin: 2px; line-height: 1.8em;background: #96005a;border: 1px solid #76003a;color: #ffffff;', 'color:blue;', 'color:red;', 'not colored'])
 })
--- packages/aurelia-logging-color/src/CSSBrush.ts
+++ packages/aurelia-logging-color/src/CSSBrush.ts
@@ -8,6 +8,12 @@ export class CSSBrush implements Brush {
   private colors: RGB[]
   private map: { [index: string]: RGB } = {}
   private option: BrushOption
+  private gammaMap = {
+    rc: 0.2126,
+    gc: 0.7152,
+    bc: 0.0722,
+    lowc: 1 / 12.92
+  }
 
   constructor(option: Partial<BrushOption> = {}) {
     this.option = {
@@ -22,17 +28,43 @@ export class CSSBrush implements Brush {
     const rgb = this.getRgb(id)
     const background = rgbHex(rgb)
     const border = rgbHex(rgb.map(x => Math.max(0, x - 32)))
-    const color = rgb.every(x => x < 220) ? '#ffffff' : '#000000'
+    const avgLuminance = rgb[0] * this.gammaMap['rc'] + rgb[1] * this.gammaMap['gc'] + rgb[2] * this.gammaMap['bc']
+    let color = this.getComplementary(rgb) // Setting the contrasting color as default
+    const bgLuminance = this.relativeLuminance(rgb)
+    const fgLuminance = (color === '#ffffff') ? this.relativeLuminance([255, 255, 255]) : this.relativeLuminance([0, 0, 0])
+    const relativeL = bgLuminance > fgLuminance ? (bgLuminance + 0.05) / (fgLuminance + 0.05) : (fgLuminance + 0.05) / (bgLuminance + 0.05)
+    if (relativeL < 4.5) { // Can set the contrast ratio based on text size later.
+      color = (avgLuminance < 220) ? '#ffffff' : '#000000' // If the relative contrast is lower than standard, we switch color to black/white
+    }
     let idStr = `%c ${id} `
     if (rest.length > 1 && typeof rest[0] === 'string' && rest[0].indexOf('%c') !== -1) {
       idStr += rest.shift()
     }
-    return [idStr, `padding: 2px; margin: 2px; line-height: 1.8em;background: ${background};bother: 1px solid ${border};color: ${color};`, ...rest]
+    return [idStr, `padding: 2px; margin: 2px; line-height: 1.8em;background: ${background};border: 1px solid ${border};color: ${color};`, ...rest]
   }
 
   private getRgb(text: string) {
-    // It is ok to overlep color.
+    // It is ok to overlap color.
     // Not trying to be too smart about it.
     return this.map[text] = this.map[text] || this.colors[this.count++ % this.option.maxColor]
+  }
+
+  private relativeLuminance(rgb: any[]) {
+    const rsrgb = rgb[0] / 255 // https://www.w3.org/TR/WCAG/#relativeluminancedef
+    const gsrgb = rgb[1] / 255
+    const bsrgb = rgb[2] / 255
+
+    const r = rsrgb <= 0.03928 ? rsrgb * this.gammaMap['lowc'] : Math.pow((rsrgb + 0.055) / 1.055, 2.4)
+    const g = gsrgb <= 0.03928 ? gsrgb * this.gammaMap['lowc'] : Math.pow((gsrgb + 0.055) / 1.055, 2.4)
+    const b = bsrgb <= 0.03928 ? bsrgb * this.gammaMap['lowc'] : Math.pow((bsrgb + 0.055) / 1.055, 2.4)
+
+    return r * this.gammaMap['rc'] + g * this.gammaMap['gc'] + b * this.gammaMap['bc']
+  }
+
+  private getComplementary(rgb: any[]) {
+    const r = 255 - rgb[0]
+    const g = 255 - rgb[1]
+    const b = 255 - rgb[2]
+    return rgbHex([r, g, b])
   }
 }
