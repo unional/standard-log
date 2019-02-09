c0527db1191a4b5a2abfc3dbe47491aeb143004b
Homa Wong (unional) <homawong@gmail.com>
2019-02-09 15:49:18 -0800

    test: imrpove getLogger tests

--- packages/standard-log/src/getLogger.spec.ts
+++ packages/standard-log/src/getLogger.spec.ts
@@ -1,5 +1,6 @@
 import a from 'assertron';
 import { getLogger } from '.';
+import { InvalidId } from './errors';
 
 test('logger id supports alphanumeric and :_-.', () => {
   getLogger('abcdefghijklmnopqrstuvwxyz:-_.1234567890')
@@ -9,7 +10,7 @@ test('logger id supports unicode', () => {
   getLogger('ミク香港')
 })
 
-test.each('`~!@#$%^&*()=+\\/|[]{}<>,?'.split(''))('throws if id has not supported character %s', (char: string) => {
-  a.throws(() => getLogger(char))
+test.each('`~!@#$%^&*()=+\\/|[]{}<>,?'.split(''))('throws if id has unsupported character %s', (char: string) => {
+  a.throws(() => getLogger(char), InvalidId)
 })
 
