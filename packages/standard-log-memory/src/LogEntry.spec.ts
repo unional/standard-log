b8f739bb4219698556071868749c2ce63ae93879
Homa Wong (unional) <homawong@gmail.com>
2019-02-11 01:47:23 -0800

    chore(memory): fix lint

--- packages/standard-log-memory/src/LogEntry.spec.ts
+++ packages/standard-log-memory/src/LogEntry.spec.ts
@@ -11,7 +11,7 @@ test('convert log entries to string', () => {
     { id: 'log', level: logLevel.notice, messages: ['notice'] },
     { id: 'log', level: logLevel.info, messages: ['info'] },
     { id: 'log', level: logLevel.debug, messages: ['debug'] },
-    { id: 'log', level: logLevel.trace, messages: ['trace'] },
+    { id: 'log', level: logLevel.trace, messages: ['trace'] }
   ])
 
   expect(actual).toEqual(`log emergency emergency
