2b9a8b6bbce7b7f43b71e483c8b97d499facb0c9
Homa Wong (unional) <homawong@gmail.com>
2019-07-11 23:21:44 -0700

    demo: add basic storybook config

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = ({ config }) => {
+  config.module.rules.push({
+    test: /\.(ts|tsx)$/,
+    use: [
+      {
+        loader: require.resolve('ts-loader'),
+        options: {
+          configFile: 'tsconfig.storybook.json'
+        }
+      },
+      // Optional
+      {
+        loader: require.resolve('react-docgen-typescript-loader'),
+      },
+    ],
+  });
+  config.resolve.extensions.push('.ts', '.tsx');
+  return config;
+};
--- packages/memory/src/MemoryLogReporter.spec.ts
+++ packages/memory/src/MemoryLogReporter.spec.ts
@@ -2,7 +2,7 @@ import { createMemoryLogReporter } from './MemoryLogReporter';
 
 test('log entries are saved in the `logs` property', () => {
   const writer = createMemoryLogReporter()
-  const entry = { loggerId: 'log', level: 1, messages: ['some messages'], timestamp: new Date() }
+  const entry = { id: 'log', level: 1, args: ['some messages'], timestamp: new Date() }
   writer.write(entry)
   expect(writer.logs).toEqual([entry])
 })
