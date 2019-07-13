2378147faaffac3104c3aca675bfdf47ba706fe9
Homa Wong (unional) <homawong@gmail.com>
2019-07-13 15:00:16 -0700

    test(memory): simplify test to run on nodejs@6

--- packages/memory/src/MemoryLogReporter.spec.ts
+++ packages/memory/src/MemoryLogReporter.spec.ts
@@ -10,7 +10,6 @@ test('log entries are saved in the `logs` property', () => {
 test('formatter can be used to pre-process the log', () => {
   const reporter = createMemoryLogReporter({
     formatter: (entry) => ({
-      ...entry,
       args: entry.args.map(arg => arg === 'secret' ? '<censored>' : arg)
     })
   })
