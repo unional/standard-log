f55c3d4abdb005e39239667fbd7bbea9372cecb3
Homa Wong (unional) <homawong@gmail.com>
2019-07-13 12:33:44 -0700

    build: update to devpkg-node@1.3.1

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,6 @@
 const base = require('@unional/devpkg-node/simple/config/jest.common')
 
 module.exports = Object.assign(base, {
-  // moduleNameMapper: {
-  //   'standard-log-(.+)$': '<rootDir>packages/$1/src'
-  // },
   projects: [
     'packages/*'
   ]
