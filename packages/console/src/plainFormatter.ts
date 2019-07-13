242d37ba0413022273251bb787e1d3e196f6c2c3
Homa Wong (unional) <homawong@gmail.com>
2019-07-13 11:08:19 -0700

    feat(console): move plainFormatter to core feat(console): move toLogLevelDisplay to core

--- packages/console/src/plainFormatter.ts
+++
@@ -1,6 +0,0 @@
-import { LogEntry } from 'standard-log-core';
-import { toLogLevelDisplay } from './utils';
-
-export function plainFormatter({ id, level, args, timestamp }: LogEntry) {
-  return [timestamp.toISOString(), id, toLogLevelDisplay(level), ...args]
-}
--- packages/console/src/utils/index.ts
+++ packages/console/src/utils/index.ts
@@ -1,2 +1,1 @@
 export * from './toConsoleMethod';
-export * from './toLogLevelDisplay';
