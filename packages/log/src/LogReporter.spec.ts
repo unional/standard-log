a0001765614b3e54c10756666029c4b13bd37300
Homa Wong (unional) <homawong@gmail.com>
2019-07-16 00:32:28 -0700

    chore(console): remove standard-log-core ref test(log): fix test as default reporter name changed

--- packages/log/src/LogReporter.spec.ts
+++ packages/log/src/LogReporter.spec.ts
@@ -1,16 +1,15 @@
+import a from 'assertron';
+import { createMemoryLogReporter } from 'standard-log-memory';
 import { getLogReporter } from '.';
 import { config } from './config';
-import { resetStore } from './store';
-import a from 'assertron'
-import { addLogReporter } from './LogReporter';
-import { createMemoryLogReporter } from 'standard-log-memory';
 import { ProhibitedDuringProduction } from './errors';
+import { addLogReporter } from './LogReporter';
+import { resetStore } from './store';
 
 describe('getLogReporter()', () => {
   test('get reporter by id', () => {
-    const reporter = getLogReporter('console')
-
-    expect(reporter!.id).toBe('console')
+    const reporter = getLogReporter('plain')
+    expect(reporter!.id).toBe('plain')
   })
 
   test('get unknown reporter gets undefined', () => {
