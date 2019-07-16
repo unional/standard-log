07188b896d96e983e08ed4efc075065bf296c2fb
Homa Wong (unional) <homawong@gmail.com>
2019-07-16 16:20:14 -0700

    chore(memor): clean up

--- packages/memory/src/MemoryLogReporter.ts
+++ packages/memory/src/MemoryLogReporter.ts
@@ -9,7 +9,7 @@ export type MemoryLogFormatter = LogFormatter<LogEntry>
 export function createMemoryLogReporter(options: LogReporterOptions<LogEntry> = {}): MemoryLogReporter {
   return {
     id: options.id || 'memory',
-    logs: [] as LogEntry[],
+    logs: [],
     write(entry) {
       if (options.filter && !options.filter(entry)) return
       this.logs.push(options.formatter ? options.formatter(entry) : entry)
