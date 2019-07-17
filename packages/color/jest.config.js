30fa7fbd122f9587004160955a272b25f081d8bb
Homa Wong (unional) <homawong@gmail.com>
2019-07-16 23:05:12 -0700

    feat(color): rename standard-log-console to standard-log-color

--- packages/color/jest.config.js
+++ packages/color/jest.config.js
@@ -1,5 +1,5 @@
 const base = require('@unional/devpkg-node/simple/config/jest.common')
 module.exports = Object.assign(base, {
-  displayName: 'console',
+  displayName: 'color',
   rootDir: '.'
 })
