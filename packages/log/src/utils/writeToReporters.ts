6d85164a484d60f618212390f5f144ea38fdcff7
Homa Wong (unional) <homawong@gmail.com>
2019-07-27 18:45:15 -0700

    test: improve coverage
    by ignoring the timed call.
    We know it is working.

--- packages/log/src/utils/writeToReporters.ts
+++ packages/log/src/utils/writeToReporters.ts
@@ -6,5 +6,6 @@ export function writeToReporters(logEntry: LogEntry) {
 }
 
 writeToReporters.fn = (logEntry: LogEntry) => {
+  // istanbul ignore next
   setImmediate(() => store.value.reporters.forEach(r => r.write(logEntry!)))
 }
