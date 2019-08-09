a6de02bcc7ccb31837188eb6b5edb821d660975c
Homa Wong (unional) <homawong@gmail.com>
2019-08-09 01:16:39 -0700

    test: fix coverage to ignore *.browser.*

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,10 @@ const base = require('@unional/devpkg-node/simple/config/jest.common')
 module.exports = Object.assign(base, {
   projects: [
     'packages/*'
+  ],
+  'collectCoverageFrom': [
+    '<rootDir>/src/**/*.[jt]s',
+    '!<rootDir>/src/bin.[jt]s',
+    '!<rootDir>/src/**/*.browser.[jt]s'
   ]
 })
--- packages/log/jest.config.js
+++ packages/log/jest.config.js
@@ -1,10 +1,5 @@
 const base = require('@unional/devpkg-node/simple/config/jest.common')
 module.exports = Object.assign(base, {
   displayName: 'log',
-  rootDir: '.',
-  'collectCoverageFrom': [
-    '<rootDir>/src/**/*.[jt]s',
-    '!<rootDir>/src/bin.[jt]s',
-    '!<rootDir>/src/**/*.browser.[jt]s'
-  ]
+  rootDir: '.'
 })
