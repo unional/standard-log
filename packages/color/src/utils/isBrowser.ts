9a468e8fb696be680138e707b050b38b4bbb2344
Homa Wong <homawong@gmail.com>
2020-12-18 16:01:53 -0800

    fix(color): remove isBrowser and use browser field

--- packages/color/src/utils/isBrowser.ts
+++
@@ -1,6 +0,0 @@
-// istanbul ignore next
-export function isBrowser() {
-  const p: any = process
-  // tslint:disable-next-line: strict-type-predicates
-  return (typeof process === 'undefined' || p.type === 'renderer' || p.browser === true || p.__nwjs)
-}
