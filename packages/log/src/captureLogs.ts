3f457c37a685435a0d20802bb0be33565e58ba20
Homa Wong <homawong@gmail.com>
2021-02-24 07:12:36 -0800

    feat: return result in captureLogs() (#97)
    * feat: return result in captureLogs()
    
    BREAKING CHANGE: return signagure of captureLogs changed
    
    * chore: update deps

--- packages/log/src/captureLogs.ts
+++ packages/log/src/captureLogs.ts
@@ -3,20 +3,20 @@ import { LogEntry, Logger, LogReporter } from './types'
 import { store } from './store'
 import { AnyFunction, isPromise } from 'type-plus'
 
-export function captureLogs(logger: Logger, funcBlock: () => Promise<any>): Promise<LogEntry[]>
-export function captureLogs(logger: Logger, funcBlock: () => void): LogEntry[]
-export function captureLogs(logger: Logger, funcBlock: AnyFunction): LogEntry[] | Promise<LogEntry[]> {
+export function captureLogs<R>(logger: Logger, funcBlock: () => Promise<R>): Promise<[R, LogEntry[]]>
+export function captureLogs<R>(logger: Logger, funcBlock: () => R): [R, LogEntry[]]
+export function captureLogs(logger: Logger, funcBlock: AnyFunction) {
   const reporter = createMemoryLogReporter()
   addRedirect(logger.id, reporter)
 
   try {
     const result = funcBlock()
     if (isPromise(result)) {
-      return result.then(() => reporter.logs)
+      return result.then((r) => [r, reporter.logs])
         .finally(() => removeRedirect(logger.id, reporter))
     }
     removeRedirect(logger.id, reporter)
-    return reporter.logs
+    return [result, reporter.logs]
   }
   catch (e) {
     removeRedirect(logger.id, reporter)
