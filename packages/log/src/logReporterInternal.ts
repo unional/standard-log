fda7209a8f72ea6cf2f8d942ed8c1dd29d3cff69
Homa Wong <homawong@gmail.com>
2021-03-19 02:37:31 -0700

    fix: support case when adding first console reporter
    from getLogger

--- packages/log/src/logReporterInternal.ts
+++ packages/log/src/logReporterInternal.ts
@@ -4,15 +4,18 @@ import { LogReporter } from './types'
 export function addLogReporterInternal(reporter: LogReporter) {
   if (reporter.isConsoleReporter) {
     const i = store.value.reporters.findIndex(r => r.isConsoleReporter)
-    const r = store.value.reporters[i]
-    store.value.reporters.splice(i, 1)
-    if (r.filter) {
-      if (reporter.filter) {
-        const f2 = reporter.filter
-        reporter.filter = entry => r.filter!(entry) && f2(entry)
-      }
-      else {
-        reporter.filter = r.filter
+    if (i >= 0) {
+      const r = store.value.reporters[i]
+      store.value.reporters.splice(i, 1)
+      if (r.filter) {
+        const f = r.filter
+        if (reporter.filter) {
+          const f2 = reporter.filter
+          reporter.filter = entry => f(entry) && f2(entry)
+        }
+        else {
+          reporter.filter = r.filter
+        }
       }
     }
   }
