9e2a811f0dab55190b29ff9d3b42165ba38f3b92
unional <homawong@gmail.com>
2022-04-03 19:44:39 -0700

    fix: allow making `config()` call during tests
    It will emit a warning.
    
    BREAKING CHANGE: config behavior changed
    
    while this shouldn't be an issue,
    marking this as a breaking change in case this causes any suprises.

--- packages/log/src/configForTest.ts
+++ packages/log/src/configForTest.ts
@@ -1,9 +1,11 @@
 import { config } from './config'
 import { createMemoryLogReporter } from './memory'
+import { store } from './store'
 import { LogLevel } from './types'
 
 
 export function configForTest(logLevel?: LogLevel) {
+  store.reset()
   const reporter = createMemoryLogReporter()
   config({
     reporters: [reporter],
--- packages/log/src/logReporter.spec.ts
+++ packages/log/src/logReporter.spec.ts
@@ -1,88 +1,99 @@
-import a from 'assertron';
-import { addLogReporter, clearLogReporters, config, createMemoryLogReporter, getLogReporter, ProhibitedDuringProduction } from '.';
-import { store } from './store';
-import { createConsoleLogReporter } from './console';
-import { LogFilter } from './types';
-import { logLevels } from './logLevel';
-import { getConsoleReporter, hasConsoleReporter } from './logReporter';
+import a from 'assertron'
+import {
+  addLogReporter, clearLogReporters, config,
+  createConsoleLogReporter, createMemoryLogReporter,
+  getConsoleReporter, getLogReporter, hasConsoleReporter,
+  LogFilter, logLevels, ProhibitedDuringProduction
+} from '.'
+import { store } from './store'
 
-beforeEach(() => {
-  store.reset()
-  config({ mode: 'test' })
+describe('production checks', () => {
+  beforeEach(() => store.reset())
+
+  describe('addLogReporter()', () => {
+    test('throws in production mode', () => {
+      config({ mode: 'production' })
+      const reporter = createMemoryLogReporter()
+      a.throws(() => addLogReporter(reporter), ProhibitedDuringProduction)
+    })
+  })
+
+  describe('clearLogReporters()', () => {
+    test('throws in production mode', () => {
+      config({ mode: 'production' })
+      a.throws(() => clearLogReporters(), ProhibitedDuringProduction)
+    })
+  })
 })
 
-describe('addLogReporter()', () => {
-  test('throws in production mode', () => {
-    config({ mode: 'production' })
-    const reporter = createMemoryLogReporter()
-    a.throws(() => addLogReporter(reporter), ProhibitedDuringProduction)
+describe('unit tests', () => {
+  beforeEach(() => {
+    store.reset()
+    config({ mode: 'test' })
   })
 
-  test('adds log reporter', () => {
-    const count = store.value.reporters.length
-    const reporter = createMemoryLogReporter()
-    addLogReporter(reporter)
+  describe('addLogReporter()', () => {
+    test('adds log reporter', () => {
+      const count = store.value.reporters.length
+      const reporter = createMemoryLogReporter()
+      addLogReporter(reporter)
 
-    expect(store.value.reporters.length).toBe(count + 1)
+      expect(store.value.reporters.length).toBe(count + 1)
+    })
+
+    describe('add another console reporter', () => {
+      test('will not replace existing one', () => {
+        expect(getLogReporter('console')).toBeDefined()
+        addLogReporter(createConsoleLogReporter({ id: 'c2' }))
+        expect(getLogReporter('c2')).toBeUndefined()
+        expect(getLogReporter('console')).toBeDefined()
+      })
+
+      test('will carry over existing filter', () => {
+        const filter: LogFilter = entry => entry.level < logLevels.alert
+        addLogReporter(createConsoleLogReporter({ id: 'c2', filter }))
+        addLogReporter(createConsoleLogReporter({ id: 'c3' }))
+        const c3 = getConsoleReporter()!
+        expect(c3.filter).toBe(filter)
+      })
+
+      test('will combine existing filter and new filter', () => {
+        addLogReporter(createConsoleLogReporter({ id: 'c2', filter: entry => entry.level > logLevels.critical }))
+        addLogReporter(createConsoleLogReporter({ id: 'c3', filter: entry => entry.level < logLevels.error }))
+        const c2 = getConsoleReporter()!
+        expect(c2.filter!({ level: logLevels.info } as any)).toBe(false)
+        expect(c2.filter!({ level: logLevels.alert } as any)).toBe(false)
+      })
+    })
   })
 
-  describe('add another console reporter', () => {
-    test('will not replace existing one', () => {
+  describe('getLogReporter()', () => {
+    test('get log reporter by id', () => {
       expect(getLogReporter('console')).toBeDefined()
-      addLogReporter(createConsoleLogReporter({ id: 'c2' }))
-      expect(getLogReporter('c2')).toBeUndefined()
-      expect(getLogReporter('console')).toBeDefined()
     })
 
-    test('will carry over existing filter', () => {
-      const filter: LogFilter = entry => entry.level < logLevels.alert
-      addLogReporter(createConsoleLogReporter({ id: 'c2', filter }))
-      addLogReporter(createConsoleLogReporter({ id: 'c3' }))
-      const c3 = getConsoleReporter()!
-      expect(c3.filter).toBe(filter)
-    })
-
-    test('will combine existing filter and new filter', () => {
-      addLogReporter(createConsoleLogReporter({ id: 'c2', filter: entry => entry.level > logLevels.critical }))
-      addLogReporter(createConsoleLogReporter({ id: 'c3', filter: entry => entry.level < logLevels.error }))
-      const c2 = getConsoleReporter()!
-      expect(c2.filter!({ level: logLevels.info } as any)).toBe(false)
-      expect(c2.filter!({ level: logLevels.alert } as any)).toBe(false)
+    test('get log reporter with unknown id gets undefined', () => {
+      expect(getLogReporter('blah')).toBeUndefined()
     })
   })
-})
 
-describe('getLogReporter()', () => {
-  test('get log reporter by id', () => {
-    expect(getLogReporter('console')).toBeDefined()
+  describe('clearLogReporters()', () => {
+    test('removes all reporters', () => {
+      clearLogReporters()
+
+      expect(store.value.reporters.length).toBe(0)
+    })
   })
 
-  test('get log reporter with unknown id gets undefined', () => {
-    expect(getLogReporter('blah')).toBeUndefined()
-  })
-})
-
-describe('clearLogReporters()', () => {
-  test('throws in production mode', () => {
-    config({ mode: 'production' })
-    a.throws(() => clearLogReporters(), ProhibitedDuringProduction)
-  })
-
-  test('removes all reporters', () => {
-    clearLogReporters()
-
-    expect(store.value.reporters.length).toBe(0)
-  })
-})
-
-describe('hasConsoleReporter()', () => {
-  test('returns true if there is a console reporter', () => {
-    expect(hasConsoleReporter()).toBeTruthy()
-  })
-  test('returns false if there is no console reporter', () => {
-    clearLogReporters()
-    expect(hasConsoleReporter()).toBeFalsy()
-    addLogReporter(createMemoryLogReporter())
-    expect(hasConsoleReporter()).toBeFalsy()
+  describe('hasConsoleReporter()', () => {
+    test('returns true if there is a console reporter', () => {
+      expect(hasConsoleReporter()).toBeTruthy()
+    })
+    test('returns false if there is no console reporter', () => {
+      clearLogReporters()
+      expect(hasConsoleReporter()).toBeFalsy()
+      addLogReporter(createMemoryLogReporter())
+      expect(hasConsoleReporter()).toBeFalsy()
+    })
   })
 })
