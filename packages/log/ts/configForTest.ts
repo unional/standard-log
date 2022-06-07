eca8627b46f7e168737bd4acb15d901e32648784
Homa Wong <homawong@gmail.com>
2022-06-07 00:46:15 -0700

    fix: remvoe ramda optional deps

--- packages/log/ts/configForTest.ts
+++ packages/log/ts/configForTest.ts
@@ -3,7 +3,6 @@ import { createMemoryLogReporter } from './memory/index.js'
 import { store } from './store.js'
 import { LogLevel } from './types.js'
 
-
 export function configForTest(logLevel?: LogLevel) {
   store.reset()
   const reporter = createMemoryLogReporter()
