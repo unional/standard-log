c4275718b4ebd0b16addeae6adacf205a21ed4bc
Homa Wong <homawong@gmail.com>
2022-06-11 21:16:05 -0700

    fix: export suppressLogs
    fix: config global when createStandardLog is called the first time

--- packages/log/ts/index.ts
+++ packages/log/ts/index.ts
@@ -1,11 +1,11 @@
 export * from './console.js'
 export * from './errors.js'
 export * from './formatter.js'
-export { configGlobal, getLogger } from './global.js'
 export * from './logLevels.js'
 export * from './memory.js'
 export {
-  createStandardLog, createStandardLogForTest, StandardLog, StandardLogInstance
+  configGlobal, createStandardLog, createStandardLogForTest,
+  getLogger, StandardLog, StandardLogInstance, suppressLogs
 } from './standardLog.js'
 export * from './types.js'
 
