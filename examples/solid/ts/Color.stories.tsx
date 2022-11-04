ce93af3ea911db9227e0c3881afab38b8901a72a
Homa Wong <homawong@gmail.com>
2022-11-04 01:56:24 -0700

    chore: add a solidjs example
    Not really related to SolidJS, just so...

---
+++ examples/solid/ts/Color.stories.tsx
@@ -0,0 +1,13 @@
+import { createStandardLog } from 'standard-log'
+import { createColorLogReporter } from 'standard-log-color'
+
+export default {
+  title: 'color'
+}
+
+export const Color = () => {
+  const sl = createStandardLog({ reporters: [createColorLogReporter()] })
+  const log = sl.getLogger('test')
+  log.info('hello world')
+  return <div>open dev tool to see the result</div>
+}
